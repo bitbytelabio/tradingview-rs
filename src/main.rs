121223a734c028ada65180adfd5aaccb643c6c0a
Jason Nguyen <jason.nguyen@tekcent.com>
2023-06-07 16:34:20 +0700

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use reqwest::{get, Client, IntoUrl, Response, Version};
-use std::collections::HashMap;
+use reqwest::get;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://apipubaws.tcbs.com.vn/tcanalysis/v1/ticker/FPT/overview";
-    let res = get(url).await?.json::<HashMap<String, String>>().await?;
-    println!("body = {:?}", res);
+    let res = get(url).await?;
+    dbg!(res);
     Ok(())
 }
