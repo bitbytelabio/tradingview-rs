379302e1d5eca261b827e1f3128989cab0028b44
Jason Nguyen <jason.nguyen@tekcent.com>
2023-06-07 16:40:35 +0700

    testing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use reqwest::get;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://apipubaws.tcbs.com.vn/tcanalysis/v1/ticker/FPT/overview";
-    let res = get(url).await?;
-    dbg!(res);
+    let res = get(url).await.unwrap();
+    let data: serde_json::Value = res.json().await.unwrap();
+    dbg!(data);
     Ok(())
 }
