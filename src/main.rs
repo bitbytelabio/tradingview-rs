cfd2420124b514090391bea6b9f8639b33cba7fa
Nguyễn Thành Đạt <dat.nguyen@gensei.io>
2023-06-07 07:31:17 +0700

    example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::header::USER_AGENT;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let res = client
+        .get("https://http3.is")
+        .version(reqwest::Version::HTTP_3)
+        .header(USER_AGENT, "reqwest")
+        .send()
+        .await?;
+    println!("{:#?}", res);
+    Ok(())
 }
