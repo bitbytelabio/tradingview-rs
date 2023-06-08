ee236328821022eaf35f2b41c81ff8412588bc06
Jason Nguyen <jason.nguyen@tekcent.com>
2023-06-08 11:27:12 +0700

    added some code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use reqwest::get;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://apipubaws.tcbs.com.vn/tcanalysis/v1/ticker/FPT/overview";
+    let url = "https://pine-facade.tradingview.com//pine-facade/list/?filter=fundamental";
     let res = get(url).await.unwrap();
     let data: serde_json::Value = res.json().await.unwrap();
     dbg!(data);
---
+++ src/utils/http_client.rs
@@ -0,0 +1,24 @@
+pub use http_client::get;
+
+const HTTP_HEADERS: vec![] = vec![
+    ("Accept", "application/json, text/plain, */*"),
+    ("Accept-Encoding", "gzip, deflate, br"),
+    ("Accept-Language", "en-US,en;q=0.9"),
+    ("Connection", "keep-alive"),
+    ("Origin", "https://www.tradingview.com"),
+    ("Referer", "https://www.tradingview.com/"),
+    ("Sec-Fetch-Dest", "empty"),
+    ("Sec-Fetch-Mode", "cors"),
+    ("Sec-Fetch-Site", "same-site"),
+    ("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 uacq"),
+];
+
+mod http_client {
+    use reqwest::Error;
+
+    pub async fn get(url: &str) -> Result<serde_json::Value, Error> {
+        let res = reqwest::get(url).await?;
+        let data: serde_json::Value = res.json().await?;
+        Ok(data)
+    }
+}
