138b4b1ff5f9059d8a5fc2b72e04c990411ec411
nguyenthdat <dat.nguyen@bitbytelab.io>
2023-06-20 21:37:37 +0700

    updated code

--- datafeed/examples/websocket.rs
+++ datafeed/examples/websocket.rs
@@ -1,8 +1,6 @@
-use futures_util::{future, pin_mut, SinkExt, StreamExt};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_tungstenite::{
     connect_async,
-    tungstenite::{client::IntoClientRequest, protocol::Message},
+    tungstenite::{client::IntoClientRequest, connect, Message},
 };
 
 #[tokio::main]
@@ -19,30 +17,34 @@ async fn main() {
     // Log the request
     tracing::debug!("request: {:?}", request);
     // Connect to the WebSocket server
-    let (ws_stream, res) = connect_async(request).await.unwrap();
+    let (mut socket, res) = connect(request).unwrap();
     // Log the response
     tracing::debug!("response: {:?}", res);
     println!("WebSocket handshake has been successfully completed");
 
-    let (mut write, read) = ws_stream.split();
-
-    let my_messsages: Vec<tungstenite::Message> = vec![
-        tungstenite::Message::Text(
-            "~m~54~m~{\"m\":\"set_auth_token\",\"p\":[\"unauthorized_user_token\"]}".to_string(),
-        ),
-        tungstenite::Message::Text(
-            "~m~34~m~{\"m\":\"set_locale\",\"p\":[\"en\",\"US\"]}".to_string(),
-        ),
-    ];
-
-    for msg in my_messsages {
-        write.send(msg).await.unwrap(); // Send each message with a 5 second interval
+    socket.write_message(Message::Text(
+        r#"{
+    "action": "authenticate",
+    "data": {
+        "key_id": "API-KEY",
+        "secret_key": "SECRET-KEY"
     }
-    let ws_to_stdout = {
-        read.for_each(|message| async {
-            let data = message.unwrap().into_data();
-            tokio::io::stdout().write_all(&data).await.unwrap();
-        })
-    };
-    ws_to_stdout.await;
+}"#
+        .into(),
+    ));
+
+    socket.write_message(Message::Text(
+        r#"{
+    "action": "listen",
+    "data": {
+        "streams": ["AM.SPY"]
+    }
+}"#
+        .into(),
+    ));
+
+    loop {
+        let msg = socket.read_message().expect("Error reading message");
+        println!("Received: {}", msg);
+    }
 }
