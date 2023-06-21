fd9c2eee38bb246056a18a0e16829e3d09bff797
nguyenthdat <dat.nguyen@bitbytelab.io>
2023-06-21 22:09:28 +0700

    updated code

--- datafeed/src/client.rs
+++ datafeed/src/client.rs
@@ -1,22 +1,22 @@
-use crate::utils::protocol::{format_ws_packet, parse_ws_packet};
-use serde_json::Value;
-use std::io::{Read, Write};
-use std::net::TcpStream;
-use tungstenite::{client::IntoClientRequest, connect, stream::MaybeTlsStream, Message, WebSocket};
-use url::Url;
-struct TVSocket {
-    socket: WebSocket<MaybeTlsStream<TcpStream>>,
-}
+// use crate::utils::protocol::{format_packet, parse_packet};
+// use serde_json::Value;
+// use std::io::{Read, Write};
+// use std::net::TcpStream;
+// use tungstenite::{client::IntoClientRequest, connect, stream::MaybeTlsStream, Message, WebSocket};
+// use url::Url;
+// struct TVSocket {
+//     socket: WebSocket<MaybeTlsStream<TcpStream>>,
+// }
 
-impl TVSocket {
-    pub fn new() -> Self {
-        let url = Url::parse("wss://data.tradingview.com/socket.io/websocket").unwrap();
-        let mut request = url.into_client_request().unwrap();
-        let headers = request.headers_mut();
-        headers.insert("Origin", "https://data.tradingview.com/".parse().unwrap());
-        let (socket, _) = connect(request).unwrap();
-        println!("WebSocket handshake has been successfully completed");
-        TVSocket { socket }
-    }
-    pub fn send() {}
-}
+// impl TVSocket {
+//     pub fn new() -> Self {
+//         let url = Url::parse("wss://data.tradingview.com/socket.io/websocket").unwrap();
+//         let mut request = url.into_client_request().unwrap();
+//         let headers = request.headers_mut();
+//         headers.insert("Origin", "https://data.tradingview.com/".parse().unwrap());
+//         let (socket, _) = connect(request).unwrap();
+//         println!("WebSocket handshake has been successfully completed");
+//         TVSocket { socket }
+//     }
+//     pub fn send() {}
+// }
