6e6a6cdc8948f5e0f0e30568ad11056f4e97bb82
Jason Nguyen <dat.nguyen@bitbytelab.io>
2023-07-04 12:58:57 +0700

    updated code

---
+++ datafeed/src/session.rs
@@ -0,0 +1,12 @@
+pub enum SessionKind {
+    QuoteSession,
+    ChartSession,
+    RelaySession,
+}
+
+impl SessionKind {
+    pub fn new(session: SessionKind) -> String {
+        //!TODO: Implement this
+        return "".to_string();
+    }
+}
