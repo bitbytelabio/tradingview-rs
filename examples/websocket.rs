7d2b8676c9084530577212f4d403c216098839aa
Jason Nguyen <dat.nguyen@bitbytelab.io>
2023-07-16 16:07:47 +0700

    update code

--- examples/websocket.rs
+++ examples/websocket.rs
@@ -1,14 +1,4 @@
-use datafeed::client::SocketMessage;
-use datafeed::utils::protocol::{format_packet, parse_packet};
-// use futures_util::SinkExt;
-use serde_json::Value;
-use tracing::debug;
-use tungstenite::{client::IntoClientRequest, connect, stream::NoDelay, Message};
-
-#[tokio::main]
-async fn main() {
+fn main() {
     // Initialize tracing subscriber
-    tracing_subscriber::fmt::init();
-
-    datafeed::quote::models::QuoteSession::load();
+    println!("Hello World!")
 }
