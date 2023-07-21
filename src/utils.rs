9af4421d78e0ac5b5cf4924ebd8df12437ccff41
Jason Nguyen <dat.nguyen@bitbytelab.io>
2023-07-21 11:43:35 +0700

    update code

--- src/utils.rs
+++ src/utils.rs
@@ -72,26 +72,41 @@ pub fn gen_session_id(session_type: &str) -> String {
     session_type.to_owned() + "_" + &result
 }
 
+/// Parses a message string into a vector of JSON values.
+///
+/// The message string may contain one or more JSON packets, each of which is
+/// preceded by a "~m~" prefix, followed by the length of the packet, and another
+/// "~m~" prefix. The function uses the `serde_json` crate to parse each packet
+/// into a JSON value, and returns a vector containing all the successful parses.
+///
+/// If the message string is empty, the function returns an error.
 pub fn parse_packet(message: &str) -> Result<Vec<Value>, Box<dyn std::error::Error>> {
     if message.is_empty() {
         return Err("Empty message".into());
     }
+
     let cleaned_message = CLEANER_RGX.replace_all(message, "");
     let packets: Vec<Value> = SPLITTER_RGX
         .split(&cleaned_message)
         .filter(|x| !x.is_empty())
-        .map(|x| {
-            let packet: Value = match serde_json::from_str(x) {
-                Ok(packet) => packet,
-                Err(e) => {
+        .map(|x| match serde_json::from_str(x) {
+            Ok(packet) => packet,
+            Err(e) => {
+                if e.is_syntax() {
+                    error!("Error parsing packet: invalid JSON: {}", e);
+                } else if e.is_eof() {
+                    error!("Error parsing packet: incomplete JSON: {}", e);
+                } else if e.is_io() {
+                    error!("Error parsing packet: I/O error: {}", e);
+                } else {
                     error!("Error parsing packet: {}", e);
-                    Value::Null
                 }
-            };
-            packet
+                Value::Null
+            }
         })
         .filter(|x| x != &Value::Null)
         .collect();
+
     Ok(packets)
 }
 
