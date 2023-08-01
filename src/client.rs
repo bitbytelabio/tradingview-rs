64180c12c53650be46f5ea7c87d97acddd495e75
Jason Nguyen <dat.nguyen@bitbytelab.io>
2023-08-01 16:11:15 +0700

    update client code

--- src/client.rs
+++ src/client.rs
@@ -74,149 +74,139 @@ fn get_screener(exchange: &str) -> Result<Screener> {
     Ok(screener)
 }
 
-pub struct Client {
-    user: User,
+async fn get(client: &User, url: &str) -> Result<Response> {
+    let cookie = format!(
+        "sessionid={}; sessionid_sign={};",
+        client.session, client.signature
+    );
+    let client: reqwest::Client = crate::utils::build_request(Some(&cookie))?;
+    let response = client.get(url).send().await?;
+    Ok(response)
 }
 
-impl Client {
-    pub fn new(user: User) -> Self {
-        Self { user: user }
-    }
+async fn post<T: serde::Serialize>(client: &User, url: &str, body: T) -> Result<Response> {
+    let cookie = format!(
+        "sessionid={}; sessionid_sign={};",
+        client.session, client.signature
+    );
+    let client: reqwest::Client = crate::utils::build_request(Some(&cookie))?;
+    let response = client.post(url).json(&body).send().await?;
+    Ok(response)
+}
 
-    async fn get(&self, url: &str) -> Result<Response> {
-        let cookie = format!(
-            "sessionid={}; sessionid_sign={};",
-            self.user.session, self.user.signature
-        );
-        let client: reqwest::Client = crate::utils::build_request(Some(&cookie))?;
-        let response = client.get(url).send().await?;
-        Ok(response)
-    }
-
-    async fn post<T: serde::Serialize>(&self, url: &str, body: T) -> Result<Response> {
-        let cookie = format!(
-            "sessionid={}; sessionid_sign={};",
-            self.user.session, self.user.signature
-        );
-        let client: reqwest::Client = crate::utils::build_request(Some(&cookie))?;
-        let response = client.post(url).json(&body).send().await?;
-        Ok(response)
-    }
-
-    #[tracing::instrument(skip(self))]
-    async fn fetch_scan_data(
-        &self,
-        tickers: Vec<String>,
-        screener: Screener,
-        columns: Vec<String>,
-    ) -> Result<serde_json::Value> {
-        let resp_body: serde_json::Value = self
-            .post(
-                &format!(
-                    "https://scanner.tradingview.com/{screener}/scan",
-                    screener = screener.to_string()
-                ),
-                serde_json::json!({
-                    "symbols": {
-                        "tickers": tickers
-                    },
-                    "columns": columns
-                }),
-            )
-            .await?
-            .json()
-            .await?;
-
-        let data = resp_body.get("data");
-        match data {
-            Some(data) => {
-                return Ok(data.clone());
-            }
-            None => return Err(Error::NoScanDataFound),
-        }
-    }
-
-    #[tracing::instrument(skip(self))]
-    pub async fn get_ta(&self, exchange: &str, symbols: &[&str]) -> Result<Vec<SimpleTA>> {
-        if exchange.is_empty() {
-            return Err(Error::ExchangeNotSpecified);
-        } else if symbols.is_empty() {
-            return Err(Error::SymbolsNotSpecified);
-        }
-        let symbols = symbols
-            .iter()
-            .map(|s| format!("{}:{}", exchange, s))
-            .collect::<Vec<String>>();
-
-        let screener = get_screener(exchange)?;
-        let cols: Vec<String> = vec!["1", "5", "15", "60", "240", "1D", "1W", "1M"]
-            .iter()
-            .map(|t| {
-                INDICATORS
-                    .iter()
-                    .map(|i| {
-                        if t != &"1D" {
-                            format!("{}|{}", i, t)
-                        } else {
-                            i.to_string()
-                        }
-                    })
-                    .collect::<Vec<String>>()
-            })
-            .flatten()
-            .collect();
-
-        match self.fetch_scan_data(symbols, screener, cols.clone()).await {
-            Ok(data) => {
-                let mut advices: Vec<SimpleTA> = vec![];
-
-                data.as_array().unwrap_or(&vec![]).iter().for_each(|s| {
-                    let mut advice = SimpleTA::default();
-                    advice.name = s["s"].as_str().unwrap_or("").to_string();
-                    s["d"]
-                        .as_array()
-                        .unwrap_or(&vec![])
-                        .iter()
-                        .enumerate()
-                        .for_each(|(i, val)| {
-                            let (name, period) =
-                                cols[i].split_once('|').unwrap_or((cols[i].as_str(), ""));
-                            let p_name = if period.is_empty() { "1D" } else { period };
-                            let name = name.split('.').last().unwrap_or(name);
-                            let val =
-                                (val.as_f64().unwrap_or(0.0) * 1000.0 / 500.0).round() / 1000.0;
-                            advice
-                                .data
-                                .entry(p_name.to_string())
-                                .or_insert_with(HashMap::new)
-                                .insert(name.to_string(), val);
-                        });
-                    advices.push(advice);
-                });
-                println!("{:#?}", advices);
-                Ok(advices)
-            }
-            Err(e) => match e {
-                Error::NoScanDataFound => {
-                    return Err(Error::SymbolsNotInSameExchange);
-                }
-                _ => {
-                    return Err(e);
-                }
+#[tracing::instrument(skip(client))]
+async fn fetch_scan_data(
+    client: &User,
+    tickers: Vec<String>,
+    screener: Screener,
+    columns: Vec<String>,
+) -> Result<serde_json::Value> {
+    let resp_body: serde_json::Value = post(
+        client,
+        &format!(
+            "https://scanner.tradingview.com/{screener}/scan",
+            screener = screener.to_string()
+        ),
+        serde_json::json!({
+            "symbols": {
+                "tickers": tickers
             },
-        }
-    }
+            "columns": columns
+        }),
+    )
+    .await?
+    .json()
+    .await?;
 
-    pub async fn search_symbol(
-        &self,
-        search: &str,
-        exchange: &str,
-        search_type: &str,
-        start: u64,
-        country: &str,
-    ) -> Result<SymbolSearch> {
-        let search_data: SymbolSearch = self
-            .get(&format!(
+    let data = resp_body.get("data");
+    match data {
+        Some(data) => {
+            return Ok(data.clone());
+        }
+        None => return Err(Error::NoScanDataFound),
+    }
+}
+
+#[tracing::instrument(skip(client))]
+pub async fn get_ta(client: &User, exchange: &str, symbols: &[&str]) -> Result<Vec<SimpleTA>> {
+    if exchange.is_empty() {
+        return Err(Error::ExchangeNotSpecified);
+    } else if symbols.is_empty() {
+        return Err(Error::SymbolsNotSpecified);
+    }
+    let symbols = symbols
+        .iter()
+        .map(|s| format!("{}:{}", exchange, s))
+        .collect::<Vec<String>>();
+
+    let screener = get_screener(exchange)?;
+    let cols: Vec<String> = vec!["1", "5", "15", "60", "240", "1D", "1W", "1M"]
+        .iter()
+        .map(|t| {
+            INDICATORS
+                .iter()
+                .map(|i| {
+                    if t != &"1D" {
+                        format!("{}|{}", i, t)
+                    } else {
+                        i.to_string()
+                    }
+                })
+                .collect::<Vec<String>>()
+        })
+        .flatten()
+        .collect();
+
+    match fetch_scan_data(client, symbols, screener, cols.clone()).await {
+        Ok(data) => {
+            let mut advices: Vec<SimpleTA> = vec![];
+
+            data.as_array().unwrap_or(&vec![]).iter().for_each(|s| {
+                let mut advice = SimpleTA::default();
+                advice.name = s["s"].as_str().unwrap_or("").to_string();
+                s["d"]
+                    .as_array()
+                    .unwrap_or(&vec![])
+                    .iter()
+                    .enumerate()
+                    .for_each(|(i, val)| {
+                        let (name, period) =
+                            cols[i].split_once('|').unwrap_or((cols[i].as_str(), ""));
+                        let p_name = if period.is_empty() { "1D" } else { period };
+                        let name = name.split('.').last().unwrap_or(name);
+                        let val = (val.as_f64().unwrap_or(0.0) * 1000.0 / 500.0).round() / 1000.0;
+                        advice
+                            .data
+                            .entry(p_name.to_string())
+                            .or_insert_with(HashMap::new)
+                            .insert(name.to_string(), val);
+                    });
+                advices.push(advice);
+            });
+            println!("{:#?}", advices);
+            Ok(advices)
+        }
+        Err(e) => match e {
+            Error::NoScanDataFound => {
+                return Err(Error::SymbolsNotInSameExchange);
+            }
+            _ => {
+                return Err(e);
+            }
+        },
+    }
+}
+
+#[tracing::instrument(skip(client))]
+pub async fn search_symbol(
+    client: &User,
+    search: &str,
+    exchange: &str,
+    search_type: &str,
+    start: u64,
+    country: &str,
+) -> Result<SymbolSearch> {
+    let search_data: SymbolSearch = get(client, &format!(
                 "https://symbol-search.tradingview.com/symbol_search/v3/?text={search}&hl=1&exchange={exchange}&lang=en&search_type={search_type}&start={start}&domain=production&sort_by_country={country}",
                 search = search,
                 exchange = exchange,
@@ -227,56 +217,59 @@ impl Client {
             .await?
             .json()
             .await?;
-        Ok(search_data)
-    }
+    Ok(search_data)
+}
 
-    pub async fn list_symbols(&self, market_type: Option<&str>) -> Result<Vec<Symbol>> {
-        let search_type = market_type.unwrap_or("");
-        let search_symbol = self.search_symbol("", "", search_type, 0, "").await?;
-        let remaining = search_symbol.remaining;
-        let mut symbols = search_symbol.symbols;
-        for i in (50..remaining).step_by(50) {
-            let search_symbol = self.search_symbol("", "", search_type, i, "").await?;
-            symbols.extend(search_symbol.symbols);
+#[tracing::instrument(skip(client))]
+pub async fn list_symbols(client: &User, market_type: Option<&str>) -> Result<Vec<Symbol>> {
+    let search_type = market_type.unwrap_or("");
+    let search_symbol_reps = search_symbol(client, "", "", search_type, 0, "").await?;
+    let remaining = search_symbol_reps.remaining;
+    let mut symbols = search_symbol_reps.symbols;
+    for i in (50..remaining).step_by(50) {
+        let search_symbol = search_symbol(client, "", "", search_type, i, "").await?;
+        symbols.extend(search_symbol.symbols);
+    }
+    Ok(symbols)
+}
+
+#[tracing::instrument(skip(client))]
+pub async fn get_chart_token(client: &User, layout_id: &str) -> Result<String> {
+    let data: serde_json::Value = get(
+        client,
+        &format!(
+            "https://www.tradingview.com/chart-token/?image_url={}&user_id={}",
+            layout_id, client.id
+        ),
+    )
+    .await?
+    .json()
+    .await?;
+
+    match data.get("token") {
+        Some(token) => {
+            return Ok(match token.as_str() {
+                Some(token) => token.to_string(),
+                None => return Err(Error::NoChartTokenFound),
+            })
         }
-        Ok(symbols)
+        None => Err("No token found").unwrap(),
     }
+}
 
-    #[tracing::instrument(skip(self))]
-    pub async fn get_chart_token(&self, layout_id: &str) -> Result<String> {
-        let data: serde_json::Value = self
-            .get(&format!(
-                "https://www.tradingview.com/chart-token/?image_url={}&user_id={}",
-                layout_id, self.user.id
-            ))
-            .await?
-            .json()
-            .await?;
-
-        match data.get("token") {
-            Some(token) => {
-                return Ok(match token.as_str() {
-                    Some(token) => token.to_string(),
-                    None => return Err(Error::NoChartTokenFound),
-                })
-            }
-            None => Err("No token found").unwrap(),
-        }
-    }
-
-    #[tracing::instrument(skip(self))]
-    pub async fn get_drawing<T>(
-        &self,
-        layout_id: &str,
-        symbol: &str,
-        chart_id: T,
-    ) -> Result<serde_json::Value>
-    where
-        T: std::fmt::Display + std::fmt::Debug,
-    {
-        let token = self.get_chart_token(layout_id).await?;
-        debug!("Chart token: {}", token);
-        let url = format!(
+#[tracing::instrument(skip(client))]
+pub async fn get_drawing<T>(
+    client: &User,
+    layout_id: &str,
+    symbol: &str,
+    chart_id: T,
+) -> Result<serde_json::Value>
+where
+    T: std::fmt::Display + std::fmt::Debug,
+{
+    let token = get_chart_token(client, layout_id).await?;
+    debug!("Chart token: {}", token);
+    let url = format!(
             "https://charts-storage.tradingview.com/charts-storage/get/layout/{layout_id}/sources?chart_id={chart_id}&jwt={token}&symbol={symbol}",
             layout_id = layout_id,
             chart_id = chart_id,
@@ -284,60 +277,64 @@ impl Client {
             symbol = symbol
         );
 
-        Ok(self.get(&url).await?.json().await?)
-    }
+    Ok(get(client, &url).await?.json().await?)
+}
 
-    #[tracing::instrument(skip(self))]
-    pub async fn get_private_indicators(&self) -> Result<Vec<IndicatorInfo>> {
-        let indicators = self
-            .get("https://pine-facade.tradingview.com/pine-facade/list?filter=saved")
+#[tracing::instrument(skip(client))]
+pub async fn get_private_indicators(client: &User) -> Result<Vec<IndicatorInfo>> {
+    let indicators = get(
+        client,
+        "https://pine-facade.tradingview.com/pine-facade/list?filter=saved",
+    )
+    .await?
+    .json::<Vec<IndicatorInfo>>()
+    .await?;
+    Ok(indicators)
+}
+
+#[tracing::instrument(skip(client))]
+pub async fn get_builtin_indicators(client: &User) -> Result<Vec<IndicatorInfo>> {
+    let indicator_types = vec!["standard", "candlestick", "fundamental"];
+    let mut indicators: Vec<IndicatorInfo> = vec![];
+    for indicator_type in indicator_types {
+        let url = format!(
+            "https://pine-facade.tradingview.com/pine-facade/list/?filter={}",
+            indicator_type
+        );
+
+        let mut data = get(client, &url)
             .await?
             .json::<Vec<IndicatorInfo>>()
             .await?;
-        Ok(indicators)
+
+        indicators.append(&mut data);
+    }
+    Ok(indicators)
+}
+
+#[tracing::instrument(skip(client))]
+pub async fn get_indicator_metadata(
+    client: &User,
+    indicator: &IndicatorInfo,
+) -> Result<IndicatorMetadata> {
+    use urlencoding::encode;
+    let url = format!(
+        "https://pine-facade.tradingview.com/pine-facade/translate/{}/{}",
+        encode(&indicator.id),
+        encode(&indicator.version)
+    );
+    debug!("URL: {}", url);
+    let data: serde_json::Value = get(client, &url).await?.json().await?;
+
+    if !data["success"].as_bool().unwrap_or(false)
+        || !data["result"]["metaInfo"]["inputs"].is_array()
+    {
+        return Err(Error::IndicatorDataNotFound(
+            data["reason"].as_str().unwrap_or_default().to_string(),
+        ));
     }
 
-    #[tracing::instrument(skip(self))]
-    pub async fn get_builtin_indicators(&self) -> Result<Vec<IndicatorInfo>> {
-        let indicator_types = vec!["standard", "candlestick", "fundamental"];
-        let mut indicators: Vec<IndicatorInfo> = vec![];
-        for indicator_type in indicator_types {
-            let url = format!(
-                "https://pine-facade.tradingview.com/pine-facade/list/?filter={}",
-                indicator_type
-            );
+    let result: IndicatorMetadata = serde_json::from_value(data["result"].clone())?;
 
-            let mut data = self.get(&url).await?.json::<Vec<IndicatorInfo>>().await?;
-
-            indicators.append(&mut data);
-        }
-        Ok(indicators)
-    }
-
-    #[tracing::instrument(skip(self))]
-    pub async fn get_indicator_metadata(
-        &self,
-        indicator: &IndicatorInfo,
-    ) -> Result<IndicatorMetadata> {
-        use urlencoding::encode;
-        let url = format!(
-            "https://pine-facade.tradingview.com/pine-facade/translate/{}/{}",
-            encode(&indicator.id),
-            encode(&indicator.version)
-        );
-        debug!("URL: {}", url);
-        let data: serde_json::Value = self.get(&url).await?.json().await?;
-
-        if !data["success"].as_bool().unwrap_or(false)
-            || !data["result"]["metaInfo"]["inputs"].is_array()
-        {
-            return Err(Error::IndicatorDataNotFound(
-                data["reason"].as_str().unwrap_or_default().to_string(),
-            ));
-        }
-
-        let result: IndicatorMetadata = serde_json::from_value(data["result"].clone())?;
-
-        Ok(result)
-    }
+    Ok(result)
 }
