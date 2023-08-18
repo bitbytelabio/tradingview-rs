95679eae5fdf23bf066c01c9a6a21b8c8a8365f7
Jason Nguyen <dat.nguyen@bitbytelab.io>
2023-08-18 11:19:52 +0700

    chore

--- examples/user.rs
+++ examples/user.rs
@@ -1,3 +1,20 @@
-fn main() {
-    print!("Hello, world!")
+use std::env;
+
+use tracing::info;
+use tradingview_rs::user::User;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let session = env::var("TV_SESSION").unwrap();
+    let signature = env::var("TV_SIGNATURE").unwrap();
+
+    let user = User::build()
+        .session(&session, &signature)
+        .get()
+        .await
+        .unwrap();
+
+    info!("User: {:?}", user.auth_token);
 }
